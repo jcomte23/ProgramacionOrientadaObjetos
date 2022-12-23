2a88cd942fbe8935c0e5e81389679edb17d58ae7
Javier Combita Tellez <javiercombita2014@gmail.com>
2022-12-23 00:22:52 -0500

    Aplicacion de encapsulamiento en Java

--- cursoPOOuber/JAVA/Car.java
+++ cursoPOOuber/JAVA/Car.java
@@ -1,19 +1,59 @@
 package JAVA;
 
 public class Car {
-    Integer id;
-    String license;
-    Account driver;
-    Integer passengers;
+    private Integer id;
+    private String license;
+    private Account driver;
+    private Integer passengers;
 
-    public Car(String license,Account driver){
-        this.license=license;
-        this.driver=driver;
+    public Car(String license, Account driver) {
+        this.license = license;
+        this.driver = driver;
     }
 
-    void printDataCar(){
-        System.out.println("license= "+ license);
-        System.out.println("driver= "+ driver.name +"/document: "+driver.document);
-        System.out.println("passengers= "+ passengers+" persons");
+    public Integer getId() {
+        return id;
     }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+
+    public Integer getPassenger() {
+        return passengers;
+    }
+
+    public void setPassenger(Integer passengers) {
+        if (passengers == 4) {
+            this.passengers = passengers;
+        } else {
+            System.out.println("nesecitas asignar 4 pasajeros");
+        }
+    }
+
+    void printDataCar() {
+        if (passengers != null) {
+            System.out.println("license= " + license);
+            System.out.println("driver= " + driver.name + "/document: " + driver.document);
+            System.out.println("passengers= " + passengers + " persons");
+        }
+
+    }
+
 }
--- cursoPOOuber/JAVA/Main.java
+++ cursoPOOuber/JAVA/Main.java
@@ -2,8 +2,8 @@ package JAVA;
 
 public class main {
     public static void main(String[] args) {
-        Car car=new Car("AMC-234",new Account("Andres Jimenez", "1096247379"));
-        car.passengers=4;
+        UberX car=new UberX("AMC-234",new Account("Andres Jimenez", "1096247379"),"Audi","Z300");
+        car.setPassenger(4);
         car.printDataCar();
     }
 }
