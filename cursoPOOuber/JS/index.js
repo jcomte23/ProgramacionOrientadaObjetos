4ff06f3b762db3f973229ab62ff08072d3bf6774
Javier Combita Tellez <javiercombita2014@gmail.com>
2022-12-22 23:31:01 -0500

    Aplicacion de herencia en js

--- cursoPOOuber/JS/index.js
+++ cursoPOOuber/JS/index.js
@@ -1,3 +1,7 @@
 var car=new Car("AMC-343",new Account("Andres Herrera","1096364784"))
 car.passengers=4
 car.printDataCar()
+
+var uberx=new UberX("AMC-343",new Account("Andres Herrera","1096364784"),"ferrari","ft500")
+uberx.passengers=4
+uberx.printDataCar()
