056cb842f4dc3a86163b6e67242cbead64e84e6c
Javier Combita Tellez <javiercombita2014@gmail.com>
2022-12-22 23:22:18 -0500

    Aplicacion de la herencia en python

--- cursoPOOuber/PYTHON/main.py
+++ cursoPOOuber/PYTHON/main.py
@@ -1,8 +1,8 @@
 from car import Car
+from UberX import Uberx
 from account import Account
 
 if __name__ == "__main__":
-    car=Car("AMC-234",Account("Andres Jimenez","190834235"))
-    car.passengers=4
-    print(vars(car))
-    print(vars(car.driver))
+    uber1=Uberx("AW456",Account("Andres Herrera", "10928378443"),"chevolet","Spark")
+    print(vars(uber1))
+    print(vars(uber1.driver))
